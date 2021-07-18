9657ce54e58b0ef0ef26f68a85cdf46e1c206259
Halil Çağrı <hcagri@path.com.tr>
2021-07-18 23:17:54 +0300

    Add - AdminLTE3 dist files to mix Add - AdminLTE3 LoginV2 page Add - Laravel TR Lang files

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+mix.copyDirectory('resources/dist', 'public/dist');
+mix.copyDirectory('resources/plugins', 'public/plugins');

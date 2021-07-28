22b1d836c66157826a39b96dc0c378dfe7a68dd5
Halil Çağrı <hcagri@path.com.tr>
2021-07-28 21:24:31 +0300

    Fix - rent form pdf button

--- resources/views/rentForms/edit.blade.php
+++ resources/views/rentForms/edit.blade.php
@@ -56,6 +56,7 @@
                         @elseif($rentForm->rentFormStatus->name == 'ACTIVE')
                             <a type="button" href="#" class="btn btn-info"><span class="fas fa-check"></span> Tamamlandı Olarak İşaretle</a>
                         @endif
+                            <a class="btn btn-success" href="{{ route('rentForms.exportPdf', ['id' => $rentForm->id]) }}" type="button"><span class="fas fa-pdf"></span> PDF Çıktı Al</a>
                     @endif
                 </div>
             </div>
